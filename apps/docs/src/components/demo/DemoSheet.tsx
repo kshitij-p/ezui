9bad9a858e4681e0e703d0301a29d54b933d7041
kshitijp <pawarkshitij888@gmail.com>
2023-06-19 23:32:14 +0530

    edit DemoSheet to test stacking dialogs

--- apps/docs/src/components/demo/DemoSheet.tsx
+++ apps/docs/src/components/demo/DemoSheet.tsx
@@ -14,6 +14,16 @@ import {
 } from "@/components/ui/Sheet";
 import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/Dialog";
 
 const DemoSheet = () => {
   const [position, setPosition] = useState<SheetPosition>("right");
@@ -44,7 +54,41 @@ const DemoSheet = () => {
           }}
         >
           <SheetHeader>
-            <SheetTitle>Edit profile</SheetTitle>
+            <SheetTitle asChild>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className="max-w-max" variants={{ type: "secondary" }}>
+                    Edit Profile
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle className="text-xl">Edit profile</DialogTitle>
+                    <DialogDescription>
+                      Make changes to your profile here. Click save when you're done.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <label htmlFor="name">Name</label>
+                      <Input variants={{ size: "sm" }} id="name" defaultValue="Pedro Duarte" className="col-span-3" />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <label htmlFor="username">Username</label>
+                      <Input variants={{ size: "sm" }} id="username" defaultValue="@peduarte" className="col-span-3" />
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button variants={{ size: "sm", type: "secondary" }}>Discard Changes</Button>
+                    </DialogClose>
+                    <Button variants={{ size: "sm" }} type="submit">
+                      Save changes
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </SheetTitle>
             <SheetDescription>Make changes to your profile here. Click save when you're done.</SheetDescription>
           </SheetHeader>
           <div className="grid gap-4 py-4">
@@ -52,13 +96,13 @@ const DemoSheet = () => {
               <label htmlFor="name" className="text-right">
                 Name
               </label>
-              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+              <Input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
               <label htmlFor="username" className="text-right">
                 Username
               </label>
-              <Input id="username" value="@peduarte" className="col-span-3" />
+              <Input id="username" defaultValue="@peduarte" className="col-span-3" />
             </div>
           </div>
           <SheetFooter>
