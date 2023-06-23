f17307ca3948d5bbf16170ad9f94d0a16319c7b6
kshitijp <pawarkshitij888@gmail.com>
2023-06-24 00:51:06 +0530

    convert next config to .mjs and add options to make builds faster

---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+
+  typescript: {
+    //TS errors will be checked in CI
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    //Eslint errors will be checked in CI
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
+};
+
+export default config;
