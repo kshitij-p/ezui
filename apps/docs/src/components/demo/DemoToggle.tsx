8f63fcef3a93d91ee06d7b9ea217f8c3bc9ac9fc
kshitijp <pawarkshitij888@gmail.com>
2023-07-02 23:47:20 +0530

    create docs for Toggle

--- apps/docs/src/components/demo/DemoToggle.tsx
+++ apps/docs/src/components/demo/DemoToggle.tsx
@@ -4,20 +4,8 @@ import { Cat } from "lucide-react";
 
 const DemoToggle = () => {
   return (
-    <div className="flex flex-col items-center gap-4">
-      <div className="flex w-full flex-wrap gap-2">
-        <Toggle>Meow</Toggle>
-        <Toggle>
-          <Cat className="h-4 w-4" />
-        </Toggle>
-        <Toggle variants={{ type: "borderless" }}>
-          <Cat className="h-4 w-4" />
-        </Toggle>
-        <Toggle disabled>
-          <Cat className="h-4 w-4" />
-        </Toggle>
-      </div>
-      <div className="flex w-full flex-wrap gap-4">
+    <div className="flex w-full flex-col items-center gap-4">
+      <div className="flex flex-wrap gap-4">
         <div className="flex flex-col gap-2">
           <Toggle variants={{ size: "sm" }}>Meow</Toggle>
           <Toggle variants={{ size: "md" }}>Meow</Toggle>
@@ -34,6 +22,18 @@ const DemoToggle = () => {
             <Cat className="h-4 w-4" />
           </Toggle>
         </div>
+      </div>
+      <div className="flex flex-wrap items-center gap-2">
+        Borderless
+        <Toggle variants={{ type: "borderless" }}>
+          <Cat className="h-4 w-4" />
+        </Toggle>
+      </div>
+      <div className="flex flex-wrap items-center gap-2">
+        Disabled
+        <Toggle disabled>
+          <Cat className="h-4 w-4" />
+        </Toggle>
       </div>
     </div>
   );
