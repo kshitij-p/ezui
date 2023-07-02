4f6958e1636a1dfd67809151cc536445e48ad2d2
kshitijp <pawarkshitij888@gmail.com>
2023-07-02 23:04:52 +0530

    create docs for Switch

--- apps/docs/src/components/demo/DemoSwitch.tsx
+++ apps/docs/src/components/demo/DemoSwitch.tsx
@@ -4,14 +4,14 @@ import { Label } from "@/components/ui/Label";
 
 const DemoSwitch = () => {
   return (
-    <div className="flex flex-wrap items-center gap-2">
+    <div className="flex w-full flex-wrap items-center justify-center gap-2">
       <div className="flex items-center space-x-2">
         <Switch id="airplaneMode" />
         <Label htmlFor="airplaneMode">Airplane Mode</Label>
       </div>
       <div className="flex items-center space-x-2">
         <Switch aria-invalid id="carMode" />
-        <Label htmlFor="carMode">Car Mode</Label>
+        <Label htmlFor="carMode">Invalid Switch</Label>
       </div>
     </div>
   );
