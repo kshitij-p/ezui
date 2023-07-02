efb389292766db3dbe8c8195b41333e6164fbde2
kshitijp <pawarkshitij888@gmail.com>
2023-07-02 21:24:54 +0530

    create docs for Button

--- apps/docs/src/components/demo/DemoButton.tsx
+++ apps/docs/src/components/demo/DemoButton.tsx
@@ -1,61 +1,47 @@
 import React from "react";
-import { Button } from "@/components/ui/Button";
+import { ALL_BUTTON_VARIANTS, Button } from "@/components/ui/Button";
 import Link from "next/link";
 
 const DemoButton = () => {
   return (
-    <div className="flex w-full flex-col items-center">
-      <div className="flex max-w-full flex-col items-center gap-8">
-        <Button variants={{ size: "xs" }}>Settings</Button>
-        <Button variants={{ size: "sm" }}>Settings</Button>
-        <Button variants={{ size: "md" }}>Settings</Button>
-        <Button variants={{ size: "lg" }}>Settings</Button>
-        <Button variants={{ size: "xl" }}>Settings</Button>
-      </div>
-      <div className="flex flex-col items-center gap-8">
-        <Button variants={{ size: "xs", type: "secondary" }}>Settings</Button>
-        <Button variants={{ size: "sm", type: "secondary" }}>Settings</Button>
-        <Button variants={{ size: "md", type: "secondary" }}>Settings</Button>
-        <Button variants={{ size: "lg", type: "secondary" }}>Settings</Button>
-        <Button variants={{ size: "xl", type: "secondary" }}>Settings</Button>
-      </div>
-      <div className="flex flex-col items-center gap-8">
-        <Button variants={{ size: "xs", type: "danger" }}>Settings</Button>
-        <Button variants={{ size: "sm", type: "danger" }}>Settings</Button>
-        <Button variants={{ size: "md", type: "danger" }}>Settings</Button>
-        <Button variants={{ size: "lg", type: "danger" }}>Settings</Button>
-        <Button variants={{ size: "xl", type: "danger" }}>Settings</Button>
-      </div>
-      <div className="flex flex-col items-center gap-8">
-        <Button variants={{ size: "xs", type: "danger-secondary" }}>Settings</Button>
-        <Button variants={{ size: "sm", type: "danger-secondary" }}>Settings</Button>
-        <Button variants={{ size: "md", type: "danger-secondary" }}>Settings</Button>
-        <Button variants={{ size: "lg", type: "danger-secondary" }}>Settings</Button>
-        <Button variants={{ size: "xl", type: "danger-secondary" }}>Settings</Button>
-      </div>
-      <div className="flex flex-col items-center gap-8">
-        <Button variants={{ size: "xs", type: "ghost" }}>Settings</Button>
-        <Button variants={{ size: "sm", type: "ghost" }}>Settings</Button>
-        <Button variants={{ size: "md", type: "ghost" }}>Settings</Button>
-        <Button variants={{ size: "lg", type: "ghost" }}>Settings</Button>
-        <Button variants={{ size: "xl", type: "ghost" }}>Settings</Button>
-      </div>
-      <div className="flex flex-col items-center gap-8">
-        <Button asChild variants={{ size: "xs", type: "primary" }}>
-          <Link href={"#"}>Link demo</Link>
-        </Button>
-        <Button asChild variants={{ size: "sm", type: "primary" }}>
-          <Link href={"#"}>Link demo</Link>
-        </Button>
-        <Button asChild variants={{ size: "md", type: "primary" }}>
-          <Link href={"#"}>Link demo</Link>
-        </Button>
-        <Button asChild variants={{ size: "lg", type: "primary" }}>
-          <Link href={"#"}>Link demo</Link>
-        </Button>
-        <Button asChild variants={{ size: "xl", type: "primary" }}>
-          <Link href={"#"}>Link demo</Link>
-        </Button>
+    <div className="flex w-full flex-wrap justify-center gap-6">
+      {Object.keys(ALL_BUTTON_VARIANTS.type).map((type) => (
+        <div key={type} className="flex flex-col items-center gap-2">
+          <h3 className="capitalize">{type}</h3>
+          <div className="flex max-w-full flex-col items-center gap-8">
+            {Object.keys(ALL_BUTTON_VARIANTS.size).map((size) => (
+              <Button
+                key={size}
+                variants={{
+                  size: size as keyof typeof ALL_BUTTON_VARIANTS.size,
+                  type: type as keyof typeof ALL_BUTTON_VARIANTS.type,
+                }}
+              >
+                Settings
+              </Button>
+            ))}
+          </div>
+        </div>
+      ))}
+      <div className="flex flex-col items-center">
+        <h3>Button as a link</h3>
+        <div className="flex max-w-full flex-col items-center gap-8">
+          <Button asChild variants={{ size: "xs", type: "primary" }}>
+            <Link href={"#"}>Link demo</Link>
+          </Button>
+          <Button asChild variants={{ size: "sm", type: "primary" }}>
+            <Link href={"#"}>Link demo</Link>
+          </Button>
+          <Button asChild variants={{ size: "md", type: "primary" }}>
+            <Link href={"#"}>Link demo</Link>
+          </Button>
+          <Button asChild variants={{ size: "lg", type: "primary" }}>
+            <Link href={"#"}>Link demo</Link>
+          </Button>
+          <Button asChild variants={{ size: "xl", type: "primary" }}>
+            <Link href={"#"}>Link demo</Link>
+          </Button>
+        </div>
       </div>
     </div>
   );
